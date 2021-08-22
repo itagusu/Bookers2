94a1ab324e7a66a334698c16d3bb253d53bea059
ir060906@gmail.com <ec2-user@ip-172-31-23-196.ec2.internal>
2021-08-22 14:37:43 +0000

    [add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,20 +5,38 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(books.id)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :new
+    end
   end
 
   def index
   end
-
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
     @book = Book.find(params[:id])
   end
 
-  def destroy
+  def update
+    book = Book.find(params[:id])
+    Book.update(book_params)
+    redirect_to book_path(book.id)
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path 
+  end
+
   private
 
   def book_params
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7efafa191336a8e5330306b00be88482ac23e0defdeeaf77e82beb3b94a6ce88b9d8dd4e307232af5213bb422e888c836803f5a823f397a7fc4ecb87dee56488'
