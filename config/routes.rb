a1e05d0e0c535cfc0e51c1bed59705bfa4017e55
ir060906@gmail.com <ec2-user@ip-172-31-23-196.ec2.internal>
2021-09-01 04:21:55 +0000

    [add]5-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users
+  resources :users do
+   resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
