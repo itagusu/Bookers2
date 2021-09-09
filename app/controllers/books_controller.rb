0506660a3b8ed589b05e14f8b2233ffe711b36e3
itagusu <ir060906@gmail.com>
2021-09-09 05:08:06 +0000

    [add]5-6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
     @user = current_user
+    #@likebook = Book.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,11 +3,15 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  #has_many :liked_users, through: :favorites, source: :user
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
+  #def self.create_all_ranks #Noteクラスからデータを取ってくる処理なのでクラスメソッド！
+    #Book.find(Favorite.group(:book_id).order('count(book_id) desc').limit(3).pluck(:book_id))
+  #end
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
